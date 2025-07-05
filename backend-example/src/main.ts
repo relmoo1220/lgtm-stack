ff03d25d1b1cdb9ed06dbeb93ad8864d659f3986
relmoo1220 <gamingwithmura@gmail.com>
2025-07-06 03:02:23 +0800

    Export logs

--- backend-example/src/main.ts
+++ backend-example/src/main.ts
@@ -9,7 +9,9 @@ async function bootstrap() {
   otelSDK.start();
   console.log('Started OTEL SDK');
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true, // When bufferLogs set to true, all logs will be buffered until a custom logger is attached
+  });
   app.useLogger(app.get(Logger)); // Use the Pino logger instead of default logger for better structured logs
 
   app.enableShutdownHooks(); // Listen for shutdown signals (SIGINT, SIGTERM, etc.)
--- backend-example/src/opentelemetry/instrumentation.ts
+++ backend-example/src/opentelemetry/instrumentation.ts
@@ -4,7 +4,7 @@ import {
   BatchLogRecordProcessor,
 } from '@opentelemetry/sdk-logs';
 import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
-import { LoggerProviderConfig, NodeSDK } from '@opentelemetry/sdk-node';
+import { NodeSDK } from '@opentelemetry/sdk-node';
 import { AsyncLocalStorageContextManager } from '@opentelemetry/context-async-hooks';
 import * as process from 'process';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
@@ -19,34 +19,37 @@ import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http';
 
 // Export logs using OTLP protocol over HTTP on port 4318
-// This is because on otel-collector-config.yaml the http receiver is on port 4318
+// This is because on otel-collector-config.yaml the http receiver for OTel is on port 4318
 const logExporter = new OTLPLogExporter({
   url: `http://${process.env.OTEL_SERVICE_NAME}:4318/v1/logs`,
 });
-const loggerProvider = new LoggerProvider({
-  processors: [new BatchLogRecordProcessor(logExporter)],
-});
+const logProcessor = new BatchLogRecordProcessor(logExporter);
 
 // Export trace using OTLP protocol over HTTP on port 4318
-// This is because on otel-collector-config.yaml the http receiver is on port 4318
+// This is because on otel-collector-config.yaml the http receiver for OTel is on port 4318
 const traceExporter = new OTLPTraceExporter({
   url: `http://${process.env.OTEL_SERVICE_NAME}:4318/v1/traces`,
 });
 const spanProcessor = new BatchSpanProcessor(traceExporter);
 
-// Export prometheus compatible metrics on port 4319
+// Export prometheus compatible metrics on port 8081 over to Mimir
 const metricReader = new PrometheusExporter({
   port: 8081,
 });
 
 const otelSDK = new NodeSDK({
-  metricReader,
-  spanProcessor: spanProcessor,
+  metricReader, // Send metrics via OTel
+  spanProcessors: [spanProcessor], // Send traces via OTel
+  logRecordProcessors: [logProcessor], // Send logs via OTel
   contextManager: new AsyncLocalStorageContextManager(),
+  // By targeting @opentelemetry/instrumentation-pino, you're enabling both:
+  // Log correlation: every Pino log gets trace_id, span_id, and trace_flags added so you can link logs to active spans
+  // Log sending: Pino logs are forwarded to your OpenTelemetry LoggerProvider and ultimately your OTLP log exporter
   instrumentations: [
     getNodeAutoInstrumentations({
       '@opentelemetry/instrumentation-pino': {
         enabled: true,
+        // Add each log record with service.name attribute
         logHook: (_span, logRecord) => {
           logRecord[ATTR_SERVICE_NAME] =
             process.env.OTEL_SERVICE_NAME || 'unknown-service';
@@ -54,6 +57,8 @@ const otelSDK = new NodeSDK({
       },
     }),
   ],
+  // A TextMapPropagator is responsible for injecting and extracting context (trace IDs, baggage) into/out of carriers like HTTP headers.
+  // CompositePropagator: You can combine multiple propagators (e.g., W3C TraceContext, Baggage, B3) so your app speaks multiple header formats.
   textMapPropagator: new CompositePropagator({
     propagators: [
       new W3CTraceContextPropagator(),
